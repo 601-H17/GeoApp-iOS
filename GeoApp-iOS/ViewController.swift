0bb8e02039c01bd26a9ba04b98735270aa09c237
MacTHEgenius <levesque.benoit@me.com>
2017-02-06 13:55:10 -0500

    Initial Commit

---
+++ GeoApp-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoApp-iOS
+//
+//  Created by Benoît Lévesque on 2017-02-06.
+//  Copyright © 2017 Genius Production. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
