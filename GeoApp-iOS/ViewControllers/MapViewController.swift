c13115d1343e724d6a5c87387ada3dcdf66f9c48
MacTHEgenius <levesque.benoit@me.com>
2017-02-06 15:53:44 -0500

    Add map view in Interface builder

--- GeoApp-iOS/ViewControllers/MapViewController.swift
+++ GeoApp-iOS/ViewControllers/MapViewController.swift
@@ -10,19 +10,25 @@ import UIKit
 import Mapbox
 
 class MapViewController: UIViewController {
+	
+	// MARK: - IB outlets
 
+	@IBOutlet var mapView: MGLMapView!
+	
+	// MARK: - View controller methods
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
 		
-		let mapView = MGLMapView(frame: self.view.bounds)
+		//let mapView = MGLMapView(frame: self.view.bounds)
 		mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
 		
 		let center = CLLocationCoordinate2D(latitude: 46.78642133499, longitude: -71.28739276585)
 		mapView.setCenter(center, zoomLevel: 16, animated: false)
 		
-		self.view.addSubview(mapView)
+		//self.view.addSubview(mapView)
     }
 
     override func didReceiveMemoryWarning() {
